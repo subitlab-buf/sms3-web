cd2796bb51a3aaaa4bc08cc24a1ca41e13dc5145
XunChestnut <creepershuai@163.com>
2023-11-24 18:46:33 +0800

    First try

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import '@arco-design/web-react/dist/css/arco.css'
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {Navigate, RouterProvider, createBrowserRouter, Routes, Route} from 'react-router-dom';
 import NormalSidebar from './components/NormalSidebar';
+
+import Login from './Templates/Login'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <NormalSidebar/>,
+    element: <Login/>,
     errorElement: <Navigate to='/'/>
   }
 ])
@@ -21,6 +24,7 @@ ReactDOM.createRoot(
     <RouterProvider router={router}/>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
