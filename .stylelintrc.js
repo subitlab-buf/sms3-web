b4095ee9c6ed56b404827c1c23b7f13d35329eb0
C191239 <zsz2007314zsz@sina.com>
2023-11-26 12:40:00 +0800

    css indentation

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
     extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
     plugins: ["stylelint-order"],
+    rules:{
+        indentation: "tab",
+    }
 }
